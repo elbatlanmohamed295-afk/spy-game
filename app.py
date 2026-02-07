0f7a35914d05dd45888c9531cd4255fef3ad1c54
elbatlanmohamed295-afk <elbatlanmohamed295@gmail.com>
2026-02-07 16:14:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,171 +1,170 @@
-import os
+from flask import Flask, render_template
+from flask_socketio import SocketIO, join_room, emit
 import random
+import string
 import time
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'el_3watly_secret_key_2026'
+app.config['SECRET_KEY'] = 'super_secret_key'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# --- قاعدة بيانات اللعبة ---
-GAME_DATA = {
-    "أماكن 🌍": ["القهوة", "القسم", "الساحل", "الأهرامات", "لجنة امتحان", "فرح شعبي", "المترو", "الجيم", "المطار", "الحلاق"],
-    "أكلات 🥘": ["كشري", "محشي", "فسيخ", "حواوشي", "كوارع", "اندومي", "ملوخية", "شاورما"],
-    "أفلام ومسلسلات 🎬": ["الكيف", "الجزيرة", "لن أعيش في جلباب أبي", "مافيا", "غبي منه فيه"],
-    "أشياء في البيت 🏠": ["الثلاجة", "الغسالة", "الريموت", "الشبشب", "الشاحن"]
+# --- قاعدة البيانات ---
+LOCATIONS = {
+    "أماكن عامة": ["المطار", "المستشفى", "الجامعة", "المترو", "المول"],
+    "أماكن ترفيهية": ["السينما", "الملاهي", "القهوة", "الجيم", "الساحل"],
+    "أماكن غريبة": ["الغواصة", "محطة فضاء", "بيت رعب", "السجن", "قاعدة عسكرية"]
 }
 
-PUNISHMENTS = ["ارقص بلدي 💃", "قلد صوت فرخة 🐔", "اعمل مذيع كورة 🎤", "غني أغنية حزينة 🐥", "10 ضغط حالاً 💪", "اعترف بحاجة محرجة 🙈"]
+PUNISHMENTS = [
+    "ارقص دقيقة بدون موسيقى", "اعترف بآخر كذبة", 
+    "اتصل بحد وقوله بحبك واقفل", "اشرب كوباية ميه كاملة", 
+    "خلي اللي جنبك يضربك بالقلم"
+]
 
-# --- إدارة الغرف (State Management) ---
-class GameRoom:
-    def __init__(self):
-        self.players = {}  # {sid: {name, img, score, role}}
-        self.spy_sid = None
-        self.location = None
-        self.category = None
-        self.start_time = None
-        self.votes = {}  # {voter_sid: target_sid}
-        self.state = "lobby" # lobby, playing, voting
-
+# --- تخزين حالة اللعبة (State Management) ---
 rooms = {}
+
+def generate_code():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# --- Socket Events ---
-
-@socketio.on('join')
-def on_join(data):
-    username = data.get('username')
-    room_code = data.get('room')
-    img = data.get('userImg')
-    
-    if not username or not room_code: return
-
-    join_room(room_code)
-    
-    if room_code not in rooms:
-        rooms[room_code] = GameRoom()
-    
-    room = rooms[room_code]
-    
-    # إضافة اللاعب
-    room.players[request.sid] = {
-        'name': username,
-        'img': img,
-        'score': 0,
-        'role': 'human'
+@socketio.on('create_room')
+def on_create(data):
+    code = generate_code()
+    rooms[code] = {
+        'players': {},      # {socket_id: name}
+        'scores': {},       # {name: score}
+        'state': 'lobby',   # lobby, playing, voting, result
+        'game_data': {},    # location, gecko_name
+        'votes': {}         # {voter_name: suspect_name}
     }
-    
-    emit_room_update(room_code)
+    join_room(code)
+    emit('room_created', {'code': code, 'name': data['name']})
 
-@socketio.on('disconnect')
-def on_disconnect():
-    for room_code, room in rooms.items():
-        if request.sid in room.players:
-            del room.players[request.sid]
-            # إذا الغرفة فضيت نحذفها
-            if not room.players:
-                del rooms[room_code]
-            else:
-                emit_room_update(room_code)
-            break
+@socketio.on('join_room')
+def on_join(data):
+    code = data['code'].upper()
+    name = data['name']
+    
+    if code in rooms:
+        if name in rooms[code]['players'].values():
+            emit('error', {'msg': 'الاسم موجود بالفعل!'})
+        else:
+            join_room(code)
+            rooms[code]['players'][request.sid] = name # ربط الاسم بالـ ID
+            rooms[code]['scores'].setdefault(name, 0)
+            
+            # إرسال تحديث لكل الناس في الغرفة
+            player_list = list(rooms[code]['players'].values())
+            emit('update_players', {'players': player_list}, room=code)
+            emit('join_success', {'code': code, 'is_host': len(player_list)==1})
+    else:
+        emit('error', {'msg': 'الغرفة غير موجودة'})
 
 @socketio.on('start_game')
 def on_start(data):
-    room_code = data['room']
-    category = data.get('category', 'أماكن 🌍')
-    timer_duration = int(data.get('timer', 300)) # بالثواني
-
-    if room_code in rooms:
-        room = rooms[room_code]
-        if len(room.players) < 3:
-            emit('error_msg', {'msg': 'محتاجين 3 لاعبين على الأقل يا كابتن!'}, to=request.sid)
-            return
-
-        # إعداد اللعبة
-        room.state = "playing"
-        room.category = category
-        room.location = random.choice(GAME_DATA[category])
-        room.votes = {}
-        
-        # اختيار البرص
-        player_sids = list(room.players.keys())
-        room.spy_sid = random.choice(player_sids)
-        
-        # إرسال البيانات للاعبين (كل واحد حسب دوره)
-        for sid in player_sids:
-            role = 'spy' if sid == room.spy_sid else 'human'
-            room.players[sid]['role'] = role
-            
-            payload = {
-                'role': role,
-                'category': category,
-                'location': room.location if role == 'human' else "???",
-                'endTime': time.time() + timer_duration
-            }
-            emit('game_started', payload, room=sid)
-
-@socketio.on('vote_player')
-def on_vote(data):
-    room_code = data['room']
-    target_sid = data['targetSid']
+    code = data['code']
+    room = rooms.get(code)
     
-    if room_code in rooms:
-        room = rooms[room_code]
-        room.votes[request.sid] = target_sid
+    if room and len(room['players']) >= 3:
+        # 1. إعداد اللعبة
+        cat = random.choice(list(LOCATIONS.keys()))
+        loc = random.choice(LOCATIONS[cat])
+        players_list = list(room['players'].values())
+        gecko = random.choice(players_list)
         
-        # التحقق إذا الكل صوت (ماعدا واحد مثلاً أو الأغلبية)
-        total_players = len(room.players)
-        total_votes = len(room.votes)
-        
-        emit('vote_update', {'votes': total_votes, 'total': total_players}, to=room_code)
+        room['game_data'] = {'location': loc, 'category': cat, 'gecko': gecko}
+        room['state'] = 'playing'
+        room['votes'] = {} # تصفير التصويت
 
-@socketio.on('reveal_result')
-def on_reveal(data):
-    room_code = data['room']
-    if room_code in rooms:
-        room = rooms[room_code]
+        # 2. إرسال الأدوار (كل واحد يعرف دوره بس)
+        for pid, pname in room['players'].items():
+            role = 'gecko' if pname == gecko else 'human'
+            info = "أنت البرص 🦎" if role == 'gecko' else f"المكان: {loc}"
+            emit('game_started', {
+                'role': role,
+                'info': info,
+                'category': cat,
+                'duration': 60 * 5 # 5 دقائق
+            }, room=pid) # إرسال خاص
         
-        # حساب التصويت
-        vote_counts = {}
-        for target in room.votes.values():
-            vote_counts[target] = vote_counts.get(target, 0) + 1
-            
-        # أكثر شخص تم التصويت ضده
-        if vote_counts:
-            victim_sid = max(vote_counts, key=vote_counts.get)
-            victim_name = room.players[victim_sid]['name']
-            is_spy_caught = (victim_sid == room.spy_sid)
-        else:
-            victim_name = "لا أحد"
-            is_spy_caught = False
+        # 3. إرسال تايمر عام
+        emit('start_timer', {'seconds': 300}, room=code)
 
-        spy_name = room.players[room.spy_sid]['name']
-        punishment = random.choice(PUNISHMENTS)
+@socketio.on('submit_vote')
+def on_vote(data):
+    code = data['code']
+    voter = data['voter']
+    suspect = data['suspect']
+    room = rooms.get(code)
+    
+    if room and room['state'] == 'playing':
+        room['votes'][voter] = suspect
+        total_players = len(room['players'])
+        current_votes = len(room['votes'])
         
-        emit('game_over', {
-            'spyName': spy_name,
-            'victimName': victim_name,
-            'spyCaught': is_spy_caught,
-            'location': room.location,
-            'punishment': punishment
-        }, to=room_code)
-        
-        room.state = "lobby"
+        # تحديث الحالة للكل (فلان صوت)
+        emit('vote_update', {
+            'voter': voter, 
+            'count': current_votes, 
+            'total': total_players
+        }, room=code)
 
-def emit_room_update(room_code):
-    if room_code in rooms:
-        players_list = [
-            {'sid': sid, 'name': p['name'], 'img': p['img'], 'isHost': i==0} 
-            for i, (sid, p) in enumerate(rooms[room_code].players.items())
-        ]
-        emit('update_player_list', {'players': players_list}, to=room_code)
+        # لو الكل صوت، ننهي اللعبة ونفرز الأصوات
+        if current_votes == total_players:
+            calculate_results(code)
+
+def calculate_results(code):
+    room = rooms[code]
+    votes = room['votes']
+    gecko = room['game_data']['gecko']
+    
+    # حساب أكثر شخص حصل على تصويت
+    vote_counts = {}
+    for suspect in votes.values():
+        vote_counts[suspect] = vote_counts.get(suspect, 0) + 1
+    
+    # من هو المشتبه به الرئيسي؟
+    top_suspect = max(vote_counts, key=vote_counts.get)
+    
+    winner = ""
+    msg = ""
+    
+    if top_suspect == gecko:
+        winner = "Humans"
+        msg = f"مبروك! قفشتوا البرص ({gecko}) 👮‍♂️"
+    else:
+        winner = "Gecko"
+        msg = f"البرص فاز! ({gecko}) هرب والناس شكت في ({top_suspect}) 🦎"
+
+    emit('game_over', {
+        'winner': winner,
+        'msg': msg,
+        'gecko_name': gecko,
+        'votes_summary': vote_counts
+    }, room=code)
+    
+    room['state'] = 'result'
+
+@socketio.on('request_punishment')
+def on_punish(data):
+    # كود العقاب كما هو
+    punishment = random.choice(PUNISHMENTS)
+    img = random.randint(1, 7)
+    emit('show_punishment', {'text': punishment, 'img': f"{img}.jpeg"}, room=data['code'])
+
+@socketio.on('reset_game')
+def on_reset(data):
+    code = data['code']
+    if code in rooms:
+        rooms[code]['state'] = 'lobby'
+        rooms[code]['votes'] = {}
+        emit('reset_to_lobby', {}, room=code)
+
+from flask import request # نسينا استدعاء request فوق
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
-
-
+    socketio.run(app, debug=True)
